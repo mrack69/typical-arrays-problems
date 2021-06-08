03228cb55a05e9e97da45ef17b9f22cddb2e427d
mrack69 <klyushentsev@gmail.com>
2021-06-08 22:16:28 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if(array.length === 0) return 0;
+  let result = array[0];
+  array.forEach(elem => {
+    if(elem < result) result = elem
+  });
+  return result
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if(array.length === 0) return 0;
+  let result = array[0];
+  array.forEach(elem => {
+    if(elem > result) result = elem
+  });
+  return result
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array =[]) {
+  if(array.length === 0) return 0;
+  let result = 0;
+  array.forEach(elem => result += elem);
+  return result / array.length
 }
